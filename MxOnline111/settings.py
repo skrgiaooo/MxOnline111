6365a15734e5d86bdccc205069cac810f6d5c8d8
skrgiaooo <64412084+skrgiaooo@users.noreply.github.com>
2020-05-08 16:30:00 +0800

    覆盖原来user表

--- MxOnline111/settings.py
+++ MxOnline111/settings.py
@@ -120,7 +120,7 @@ USE_L10N = True
 
 USE_TZ = False
 
-
+AUTH_USER_MODEL = 'users.UserProfile'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from datetime import datetime
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+GENDER_CHOICES =(
+    ('male','男'),
+    ('female','女')
+)
+class BaseModel(models.Model):
+    add_time = models.DateTimeField(default=datetime.now,verbose_name='添加时间')
+    class Meta:
+        abstract = True#将该基类定义为抽象类，即不生成表单，只作为可以继承的基类
+class UserProfile(AbstractUser):
+    nick_name = models.CharField(max_length=100,verbose_name='昵称')
+    birthday = models.DateField(verbose_name='生日',null=True,blank=True)
+    gender = models.CharField(max_length=6,verbose_name='性别',choices=GENDER_CHOICES)
+    address = models.CharField(max_length=100,verbose_name='地址')
+    mobile = models.CharField(max_length=11,verbose_name='手机号')
+    image = models.ImageField(verbose_name='上传头像',upload_to='head_image/%M/%M',default='default.jpg')
+    class Meta:
+        verbose_name = '用户信息'
+        verbose_name_plural = verbose_name
+    def unread_nums(self):
+        return self.usermessage_set.filter(has_read=False).count()
+    def __str__(self):
+        if self.nick_name:
+            return self.nick_name
+        else:
+            return self.username
