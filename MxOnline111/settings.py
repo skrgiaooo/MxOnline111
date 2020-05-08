9c57845a3235a4608825e7119daf2466fbe14464
skrgiaooo <64412084+skrgiaooo@users.noreply.github.com>
2020-05-08 19:12:52 +0800

    xadmin的配置

--- MxOnline111/settings.py
+++ MxOnline111/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.users.apps.UsersConfig',
     'apps.courses.apps.CoursesConfig',
+    'crispy_forms',
+    'xadmin.apps.XAdminConfig'
 ]
 
 MIDDLEWARE = [
